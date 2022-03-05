384df404e758c5e835124e2329c3d93f4f471d54
roger <rogeriolucas@gmail.com>
2022-03-05 16:51:25 -0300

    send macbook temperature (test)

--- test.sh
+++ test.sh
@@ -4,16 +4,32 @@
 temp1=38 #$(((RANDOM % 5) + 40))
 temp2=38 #$(((RANDOM % 5) + 40))
 
-while [ 1 ]; do
-  sensor1="$(($temp1 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
-  sensor2="$(($temp2 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
+if [ -f /tmp/powermetrics.txt ]; then
+    rm /tmp/powermetrics.txt
+    mkfifo /tmp/powermetrics.txt
+fi
 
-  curl -X 'POST' 'http://causaefeito.rlrobotics.com:8000/api/v1/sensor' -H 'accept: application/json' -H 'Content-Type: application/json' -d "{
-  \"name\": \"string\",
-  \"sensor1\": $sensor1,
-  \"sensor2\": $sensor2,
-  \"description\": \"string\"}"
+if [ -z "$(ps -ef | grep -v grep | grep powermetrics)" ]; then
+    echo "start powermetrics!"
+    sudo powermetrics --sample-rate=500 -o /tmp/powermetrics.txt &
+fi
 
-  sleep 1
-done
+while read -r data; do
 
+    sensor1=$(echo $data | awk '/CPU die temperature/ {print $4}')
+    if [ -z "$sensor1" ]; then
+        continue
+    fi
+
+    #echo "#### $sensor1"
+
+    #sensor1="$(($temp1 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
+    sensor2="$(($temp2 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
+
+    curl -X 'POST' 'http://causaefeito.rlrobotics.com:8000/api/v1/sensor' -H 'accept: application/json' -H   'Content-Type: application/json' -d "{
+    \"name\": \"string\",
+    \"sensor1\": $sensor1,
+    \"sensor2\": $sensor2,
+    \"description\": \"string\"}"
+
+done < /tmp/powermetrics.txt
