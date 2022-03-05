c30c32dbff226e6c88d7febda9f594c274f9c214
roger <rogeriolucas@gmail.com>
2022-03-05 15:59:50 -0300

    add test script

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-temp1=$(((RANDOM % 10) + 1))
-temp2=$(((RANDOM % 10) + 1))
+temp1=38 #$(((RANDOM % 5) + 40))
+temp2=38 #$(((RANDOM % 5) + 40))
 
 while [ 1 ]; do
-  sensor1="$(($temp1 + (RANDOM % 3) )).$(((RANDOM % 50) + 1))"
-  sensor2="$(($temp2 + (RANDOM % 3) )).$(((RANDOM % 50) + 1))"
+  sensor1="$(($temp1 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
+  sensor2="$(($temp2 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
 
   curl -X 'POST' 'http://causaefeito.rlrobotics.com:8000/api/v1/sensor' -H 'accept: application/json' -H 'Content-Type: application/json' -d "{
   \"name\": \"string\",
