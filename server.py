df8026511d32c44b3ef1346cc10417ef3c4ccf1d
roger <rogeriolucas@gmail.com>
2022-03-05 11:38:53 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author: Causa Efeito
+Date: 2022/03
+"""
+
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+
+
+class Sensor(BaseModel):
+    name: str
+    value: float
+    value2: Optional[float] = None
+    description: Optional[str] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/api/v1/sensor")
+async def create_item(sensor: Sensor) -> str:
+
+    data = sensor.dict()
+    print(f"post data: {data}")
+
+    return "ok"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
