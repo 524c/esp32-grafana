c75279fdf18ebc8865fe0c427675df8b28d51336
roger <rogeriolucas@gmail.com>
2022-03-05 17:02:23 -0300

    more tests..

--- test.sh
+++ test.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-temp1=38 #$(((RANDOM % 5) + 40))
-temp2=38 #$(((RANDOM % 5) + 40))
+temp1=$(((RANDOM % 10) + 40))
+temp2=$(((RANDOM % 10) + 30))
 
-if [ -f /tmp/powermetrics.txt ]; then
-    rm /tmp/powermetrics.txt
-    mkfifo /tmp/powermetrics.txt
-fi
+while [ 1 ]; do
 
-if [ -z "$(ps -ef | grep -v grep | grep powermetrics)" ]; then
-    echo "start powermetrics!"
-    sudo powermetrics --sample-rate=500 -o /tmp/powermetrics.txt &
-fi
-
-while read -r data; do
-
-    sensor1=$(echo $data | awk '/CPU die temperature/ {print $4}')
-    if [ -z "$sensor1" ]; then
-        continue
-    fi
-
-    #echo "#### $sensor1"
-
-    #sensor1="$(($temp1 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
+    sensor1="$(($temp1 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
     sensor2="$(($temp2 + (RANDOM % 5))).$(((RANDOM % 50) + 1))"
 
     curl -X 'POST' 'http://causaefeito.rlrobotics.com:8000/api/v1/sensor' -H 'accept: application/json' -H   'Content-Type: application/json' -d "{
@@ -32,4 +15,5 @@ while read -r data; do
     \"sensor2\": $sensor2,
     \"description\": \"string\"}"
 
-done < /tmp/powermetrics.txt
+    sleep 0.5
+done
